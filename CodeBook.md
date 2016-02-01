218015d6b5124424f1e7df5f2c2c3a643dd17476
sid8491 <sid.8491@gmail.com>
2016-02-02 02:11:54 +0530

    completed

--- CodeBook.md
+++ CodeBook.md
@@ -1,14 +1,14 @@
-## Introduction
+# Code Book
 
-The script run_analysis.R performs the 5 steps described in the course project's definition.
+### The script run_analysis.R performs the 5 steps described in the course project's definition.
 
    * First, all the similar data is merged using the rbind() function. By similar, we address those files having the same number of columns and referring to the same entities.
    * Then, only those columns with the mean and standard deviation measures are taken from the whole dataset. After extracting these columns, they are given the correct names, taken from features.txt.
    * As activity data is addressed with values 1:6, we take the activity names and IDs from activity_labels.txt and they are substituted in the dataset.
    * On the whole dataset, those columns with vague column names are corrected.
-   * Finally, we generate a new dataset with all the average measures for each subject and activity type (30 subjects * 6 activities = 180 rows). The output file is called tidy.txt, and uploaded to this repository.
+   * Finally, we generate a new dataset with all the average measures for each subject and activity type (30 subjects * 6 activities = 180 rows). The output file is called tidy.txt
 
-Variables
+### Code Variables
 
    * trainData, trainLabels, testData, testLabels, trainSubject and testSubject contain the data from the downloaded files.
    * trainData, trainLabels and trainSubject merge the previous datasets to further analysis.
@@ -16,3 +16,29 @@ Variables
    * A similar approach is taken with activity names through the activities variable.
    * allData merges trainData, trainLabels and trainSubject in a big dataset.
    * Finally, tidy contains the relevant averages which will be later stored in a .txt file. ddply() from the plyr package is used to apply colMeans() and ease the development.
+
+### Measurements
+
+"tBodyAccMeanX" "tBodyAccMeanY" "tBodyAccMeanZ" "tBodyAccStdX" "tBodyAccStdY" "tBodyAccStdZ" "tGravityAccMeanX" "tGravityAccMeanY" "tGravityAccMeanZ" "tGravityAccStdX" 
+"tGravityAccStdY" "tGravityAccStdZ" "tBodyAccJerkMeanX" "tBodyAccJerkMeanY" "tBodyAccJerkMeanZ" "tBodyAccJerkStdX" "tBodyAccJerkStdY" "tBodyAccJerkStdZ" "tBodyGyroMeanX" 
+"tBodyGyroMeanY" "tBodyGyroMeanZ" "tBodyGyroStdX" "tBodyGyroStdY" "tBodyGyroStdZ" "tBodyGyroJerkMeanX" "tBodyGyroJerkMeanY" "tBodyGyroJerkMeanZ" "tBodyGyroJerkStdX" 
+"tBodyGyroJerkStdY" "tBodyGyroJerkStdZ" "tBodyAccMagMean" "tBodyAccMagStd" "tGravityAccMagMean" "tGravityAccMagStd" "tBodyAccJerkMagMean" "tBodyAccJerkMagStd" 
+"tBodyGyroMagMean" "tBodyGyroMagStd" "tBodyGyroJerkMagMean" "tBodyGyroJerkMagStd" "fBodyAccMeanX" "fBodyAccMeanY" "fBodyAccMeanZ" "fBodyAccStdX" "fBodyAccStdY" 
+"fBodyAccStdZ" "fBodyAccJerkMeanX" "fBodyAccJerkMeanY" "fBodyAccJerkMeanZ" "fBodyAccJerkStdX" "fBodyAccJerkStdY" "fBodyAccJerkStdZ" "fBodyGyroMeanX" "fBodyGyroMeanY" 
+"fBodyGyroMeanZ" "fBodyGyroStdX" "fBodyGyroStdY" "fBodyGyroStdZ" "fBodyAccMagMean" "fBodyAccMagStd" "fBodyBodyAccJerkMagMean" "fBodyBodyAccJerkMagStd" "fBodyBodyGyroMagMean" 
+"fBodyBodyGyroMagStd" "fBodyBodyGyroJerkMagMean" "fBodyBodyGyroJerkMagStd"
+
+### Identifiers
+
+   * subject - The ID of the test subject
+   * activity - The type of activity performed when the corresponding measurements were taken
+
+### Activity Labels
+
+   * WALKING (value 1): subject was walking during the test
+   * WALKING_UPSTAIRS (value 2): subject was walking up a staircase during the test
+   * WALKING_DOWNSTAIRS (value 3): subject was walking down a staircase during the test
+   * SITTING (value 4): subject was sitting during the test
+   * STANDING (value 5): subject was standing during the test
+   * LAYING (value 6): subject was laying down during the test
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,9 @@
-library(plyr)
-
 # setting filenames
 filezip <- "UCI_dataset.zip"
 filename <- "UCI HAR Dataset"
 filepath <- file.path(getwd(), filename)
 
-# downloading and unzipping the dataset
+# downloading and unzipping the dataset if not present
 if (!file.exists(filezip)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileURL, filezip, method="curl")
@@ -15,41 +13,56 @@ if (!file.exists(filename)) {
 }
 
 # read all the necessary files
-testSubject <- tbl_df(read.table(file.path(filepath, "test" , "subject_test.txt" )))
-testData <- tbl_df(read.table(file.path(filepath, "test" , "X_test.txt" )))
-testLabels <- tbl_df(read.table(file.path(filepath, "test" , "y_test.txt" )))
+testSubject <- read.table(file.path(filepath, "test" , "subject_test.txt" ))
+testData <- read.table(file.path(filepath, "test" , "X_test.txt" ))
+testLabels <- read.table(file.path(filepath, "test" , "y_test.txt" ))
   
-trainSubject <- tbl_df(read.table(file.path(filepath, "train", "subject_train.txt")))
-trainData <- tbl_df(read.table(file.path(filepath, "train", "X_train.txt")))
-trainLabels <- tbl_df(read.table(file.path(filepath, "train", "y_train.txt")))
-
-dataFeatures <- tbl_df(read.table(file.path(filepath, "features.txt")))
-activity <- tbl_df(read.table(file.path(filepath, "activity_labels.txt")))
-
+trainSubject <- read.table(file.path(filepath, "train", "subject_train.txt"))
+trainData <- read.table(file.path(filepath, "train", "X_train.txt"))
+trainLabels <- read.table(file.path(filepath, "train", "y_train.txt"))
 
 # Merges the training and the test sets to create one data set
 allSubject <- rbind(trainSubject, testSubject)
 allData <- rbind(trainData, testData)
 allLabels <- rbind(trainLabels, testLabels)
-colnames(allData) <- dataFeatures$V2
 
-
-# Extracts only the measurements on the mean and standard deviation for each measurement
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+dataFeatures <- read.table(file.path(filepath, "features.txt"))
 mean_and_std_features <- grep("-(mean|std)\\(\\)", dataFeatures$V2)
 allData <- allData[, mean_and_std_features]
 
+names(allData) <- gsub("\\(\\)", "", dataFeatures[mean_and_std_features, 2]) # remove "()"
+names(allData) <- gsub("mean", "Mean", names(allData)) # capitalize M
+names(allData) <- gsub("std", "Std", names(allData)) # capitalize S
+names(allData) <- gsub("-", "", names(allData)) # remove "-" in column names 
 
 # Uses descriptive activity names to name the activities in the data set
+activity <- read.table(file.path(filepath, "activity_labels.txt"))
 allLabels$V1 <- activity[allLabels$V1, 2]
+names(allLabels) <- "activity"
 
-
-# Appropriately labels the data set with descriptive variable names
-colnames(allLabels) <- "activity"
+# Appropriately labels the data set with descriptive activity names
 names(allSubject) <- "subject"
+tidy_data <- cbind(allSubject, allLabels, allData)
 
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject
+subjectLen <- length(table(allSubject)) # 30
+activityLen <- dim(activity)[1] # 6
+columnLen <- dim(tidy_data)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(tidy_data)
+row <- 1
+for(i in 1:subjectLen) {
+    for(j in 1:activityLen) {
+        result[row, 1] <- sort(unique(allSubject)[, 1])[i]
+        result[row, 2] <- activity[j, 2]
+        bool1 <- i == tidy_data$subject
+        bool2 <- activity[j, 2] == tidy_data$activity
+        result[row, 3:columnLen] <- colMeans(tidy_data[bool1&bool2, 3:columnLen])
+        row <- row + 1
+    }
+}
+# dim(result) --> 180 68
 
-# creates a second, independent tidy data set with the average of each variable for each activity and each subject
-tidy_data <- ddply(allData, 1:66, function(x) colMeans(x[, 1:66]))
-tidy_data <- cbind(allData, allLabels, allSubject)
-
-write.table(tidy_data, "tidy.txt", row.names = FALSE, quote = FALSE)
+write.table(result, "tidy.txt", row.names=FALSE)
